828da8c9cef3d4119db0b78574be29f52cc67c1d
fabian-dietrich <exon-table1c@icloud.com>
2026-03-30 17:08:14 +0200

    add stack assets for project page, first draft at shitfy project page

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         datenschutz: resolve(__dirname, 'datenschutz.html'),
+        shifty: resolve(__dirname, 'shifty.html'),
       },
     },
   },
